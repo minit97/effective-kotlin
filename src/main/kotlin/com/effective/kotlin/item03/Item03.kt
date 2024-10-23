b11897cf44589a22f50f5dd14188f5cb2e3d0447
minit <dndb4902@naver.com>
2024-10-23 19:37:05 +0900

    feat: item03 code

---
+++ src/main/kotlin/com/effective/kotlin/item03/Item03.kt
@@ -0,0 +1,18 @@
+package com.effective.kotlin.item03
+
+class User(val name: String)
+
+class UserRepo(
+    val user: User = User("John Doe")
+)
+
+/** 1. 최대한 플랫폼 타입을 사용하지 말라
+ */
+fun example01() {
+    // 위의 전제들은 Java로 간주해야 아래 내용들을 이해할 수 있다.
+
+    val repo = UserRepo()
+    val user1 = repo.user           // user1의 타입은 User!
+    val user2: User = repo.user     // user2의 타입은 User
+    val user3: User? = repo.user    // user3의 타입은 User?
+}
