630b2d7b3beb9bfe9c4f008804c8c46edd55c7fc
minit <dndb4902@naver.com>
2024-11-10 15:09:21 +0900

    feat: item09

---
+++ src/main/kotlin/com/effective/kotlin/item09/Item09.kt
@@ -0,0 +1,32 @@
+package com.effective.kotlin.item09
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+
+
+/** 1. use
+ */
+fun countCharactersInFile1(path: String): Int {
+//    val reader = BufferedReader(FileReader(path))
+//    reader.use {
+//        return reader.lineSequence().sumOf { it.length }
+//    }
+
+    BufferedReader(FileReader(path)).use { reader ->
+        return reader.lineSequence().sumOf { it.length }
+    }
+}
+
+/** 2. useLines
+ */
+fun countCharactersInFile2(path: String): Int {
+    File(path).useLines { lines ->
+        return lines.sumOf { it.length }
+    }
+}
+
+fun countCharactersInFile2_refactor(path: String): Int =
+    File(path).useLines { lines ->
+        lines.sumOf { it.length }
+    }
